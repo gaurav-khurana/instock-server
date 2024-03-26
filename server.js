cf62b56f6ca5b3396c49569aece16df46872a162
Gaurav <program.drgk@gmail.com>
2024-03-25 21:25:27 -0700

    Port setup DONE; Middleware for cors, json DONE; Routes mock file & folder DONE

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+
+// define port
+const { PORT } = process.env;
+
+// middleware for cors
+app.use("cors");
+
+// middleware for JSON format
+app.use(express.json());
+
+// routes path
+// place here
+
+// listen to server
+app.use(PORT, () => {
+  console.log("Listening to port ", PORT);
+  console.log("To Quit server - Ctrl + C");
+});
