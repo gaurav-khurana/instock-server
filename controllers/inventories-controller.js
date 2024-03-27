b393ced882c537a09af05828ed8d0ac64a910b9f
Gaurav <program.drgk@gmail.com>
2024-03-27 10:42:28 -0700

    Back-end GET Single Inventory Item DONE

--- controllers/inventories-controller.js
+++ controllers/inventories-controller.js
@@ -3,8 +3,22 @@ const knex = require("knex")(require("../knexfile"));
 // func to Get Single Inventory Item by Id
 
 const getSingleInventory = async (req, res) => {
-  const response = await knex("inventories").where({ id: req.params.id });
-  console.log(response);
+  try {
+    const response = await knex("inventories").where({ id: req.params.id });
+    console.log(response);
+
+    if (response.length === 0) {
+      res.status(404).json({
+        message: `No inventory of id ${req.params.id} exists. Invalid id`,
+      });
+    }
+    const foundInventory = response[0];
+    res.status(200).json({ foundInventory });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: `Can't get individual inventory ${error}` });
+  }
 };
 
 module.exports = {
--- routes/routes-inventories.js
+++ routes/routes-inventories.js
@@ -1,4 +1,4 @@
-const router = require("express").Router;
+const router = require("express").Router();
 require("dotenv").config();
 const inventories_controller = require("../controllers/inventories-controller");
 
