00162206fc3e95b1adfb3a22ce341921388fa946
mortezatava <morteza.tavakoli11@gmail.com>
2024-03-27 12:34:54 -0700

    done getting all inventories in back-end

--- routes/routes-inventories.js
+++ routes/routes-inventories.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const inventories_controller = require("../controllers/inventories-controller");
 
 // route for inventories
-
+router.route("/inventories/").get(inventories_controller.getAllInventories);
 // route for Get Single Inventory Item
 router.route("/inventories/:id").get(inventories_controller.getSingleInventory);
 
