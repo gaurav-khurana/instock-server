0c64c3877ea5be14586e267ff0daf4f55f507c1d
Gaurav <program.drgk@gmail.com>
2024-03-26 17:41:11 -0700

    Delete Warehouse using Delete method DONE

--- routes/routes-warehouse.js
+++ routes/routes-warehouse.js
@@ -2,9 +2,13 @@ const router = require("express").Router();
 const { Router } = require("express");
 const warehouse_controller = require("../controllers/warehouse-controller");
 
+// route to get all warehouses, add new warehouse
 router
   .route("/warehouses")
   .get(warehouse_controller.getAllWarehouses)
   .post(warehouse_controller.addWarehouse);
+
+// route to delete warehouse
+router.route("/warehouses/:id").delete(warehouse_controller.deleteWarehouse);
 
 module.exports = router;
