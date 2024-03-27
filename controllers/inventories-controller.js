b5c8aac2f858d5627483e684eb8e22a67b2b779f
Gaurav <program.drgk@gmail.com>
2024-03-27 10:27:18 -0700

    Initial setup for Get Single Inventory

--- controllers/inventories-controller.js
+++ controllers/inventories-controller.js
@@ -1,1 +1,12 @@
 const knex = require("knex")(require("../knexfile"));
+
+// func to Get Single Inventory Item by Id
+
+const getSingleInventory = async (req, res) => {
+  const response = await knex("inventories").where({ id: req.params.id });
+  console.log(response);
+};
+
+module.exports = {
+  getSingleInventory,
+};
--- routes/routes-inventories.js
+++ routes/routes-inventories.js
@@ -4,4 +4,7 @@ const inventories_controller = require("../controllers/inventories-controller");
 
 // route for inventories
 
+// route for Get Single Inventory Item
+router.route("/inventories/:id").get(inventories_controller.getSingleInventory);
+
 module.exports = router;
