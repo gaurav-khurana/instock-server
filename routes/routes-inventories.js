aca16fa01094b5ac13ee7f3a18351ec6b6282ca3
unknown <genesisgithub@gmail.com>
2024-03-27 13:24:18 -0700

    Back-End: API to POST/CREATE a New Inventory Item [DONE]

--- routes/routes-inventories.js
+++ routes/routes-inventories.js
@@ -3,6 +3,7 @@ require("dotenv").config();
 const inventories_controller = require("../controllers/inventories-controller");
 
 // route for inventories
+router.route("/inventories").post(inventories_controller.addInventory);
 
 // route for Get Single Inventory Item
 router.route("/inventories/:id").get(inventories_controller.getSingleInventory);
